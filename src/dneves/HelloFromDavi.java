bd7c28262268d79d0183d0429eaee05e7ec02f35
Thomas Weaver <tjayweaver@gmail.com>
2020-10-09 18:21:41 -0400

    edits from Thomas

--- src/dneves/HelloFromDavi.java
+++ src/dneves/HelloFromDavi.java
@@ -5,5 +5,6 @@ public class HelloFromDavi {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Davi.");
+        System.out.println("Hello Davi, my name is Thomas");
     }
 }
